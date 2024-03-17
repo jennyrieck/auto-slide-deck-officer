592e3371790283d3eaee22fa3c78954db7e2cb34
jennyrieck <jenny.rieck@gmail.com>
2024-03-17 13:31:38 -0400

    chore: cleanup unused code/slides

--- generate_slide_deck_officer.R
+++ generate_slide_deck_officer.R
@@ -36,10 +36,10 @@ oi_colors <- as.vector(palette.colors()[2:9])
 ppt_colors <- c("#4F81BD", "#C0514D", "#9BBB59", "#8064A2", "#4bABC6")
 gbr_colors <- c("#9BBB59","#4F81BD", "#C0514D", "#8064A2", oi_colors)
 
-#################
+########################
 ## SLIDE 1 bullet points
-#################
-## S1 TEXT
+########################
+
 slide1_notes <- "Slide 1 features bulleted text and an image. 
 unordered_list() can be used to create bulletpoints with different levels. 
 external_img() allows you to insert image files into a picture location in your pptx template."
@@ -54,7 +54,6 @@ slide1_bullets <- unordered_list(
 
 image_file <- external_img("mr_jones.png")
 
-## S1 format
 ppt_out <- add_slide(ppt_out, layout = "slide1", master = "Theme1") %>%
   ph_with(value = slide1_txt_title, location = ph_location_label(ph_label = "title")) %>%
   ph_with(value = slide1_txt_bodytxt,location = ph_location_label(ph_label = "bodytext")) %>%
@@ -62,27 +61,9 @@ ppt_out <- add_slide(ppt_out, layout = "slide1", master = "Theme1") %>%
   ph_with(value = image_file, location = ph_location_label(ph_label = "image")) %>%
   set_notes(value = slide1_notes, location = notes_location_type("body"))
 
-
-
-# #################
-# ## SLIDE 1 alt graphic text
-# #################
-# 
-# ppt_out <- add_slide(ppt_out, layout = "slide1_alt", master = "Theme1") %>%
-#   ph_with(value = "Cat Encounters", location = ph_location_label(ph_label = "title")) %>%
-#   ph_with(value = report_type,location = ph_location_label(ph_label = "subtitle1")) %>%
-#   ph_with(value = paste(start_date, "to", end_date),location = ph_location_label(ph_label = "subtitle2")) %>%
-#   ph_with(value = paste0(CE_txt$Slide_Text[1], start_date, " to ", end_date), 
-#           location = ph_location_label(ph_label = "textbox0")) %>%
-#   ph_with(value = CE_txt$Slide_Text[2], location = ph_location_label(ph_label = "textbox1")) %>%
-#   ph_with(value = CE_txt$Slide_Text[3], location = ph_location_label(ph_label = "textbox2")) %>%
-#   ph_with(value = CE_txt$Slide_Text[4], location = ph_location_label(ph_label = "textbox3")) %>%
-#   ph_with(value = CE_txt$Slide_Text[5], location = ph_location_label(ph_label = "textbox4")) %>%
-#   set_notes(value = slide1_notes, location = notes_location_type("body"))
-
-# #################
+# ########################
 # ## SLIDE 2 ggplot + mschart
-# #################
+# ########################
 
 slide2_notes <- "Slide 2 features editable plots made with ggplot2 and mschart.
 Wrapping the ggplot in dml() turn the ggplot into graphic vectors in which design and text can be edited (left plot). 
@@ -99,21 +80,7 @@ CE_month_type_long <- CE_month_type %>%
   pivot_longer(cols = 4:(dim(CE_month_type)[2]), names_to = "type", values_to = "count") %>%
   unite("time", FY:Month, remove = FALSE)
 
-# CE_quarter_type_long <- CE_month_type_long %>%
-#   group_by(Quarter, type) %>%
-#   summarize(`CE Count` = sum(count)) %>% ungroup()
-# 
-# slide3_ms_bars <- ms_barchart(data = CE_quarter_type_long,
-#                               x = "Quarter", y = "CE Count", group = "type")
-# slide3_ms_bars <- chart_settings( x = slide3_ms_bars,
-#                                   dir="vertical", grouping="clustered", gap_width = 150 )
-# 
-# ppt_out <- add_slide(ppt_out, layout = "slide2_alt", master = "Theme1") %>%
-#   ph_with(value = slide3_ms_bars, location = ph_location_label(ph_label = "graph1"))
-
-#########################
-### s2 CHART 1 STACKED BARS X TIME
-
+### s2 ggplot
 ## https://stackoverflow.com/questions/20571306/multi-row-x-axis-labels-in-ggplot-line-chart
 ## https://github.com/tidyverse/ggplot2/issues/2933
 gg_vbar_month_by_type <- ggplot(CE_month_type_long, aes(y = count, x = Month, fill = reorder(type, count, sum))) + 
@@ -138,26 +105,10 @@ gg_vbar_month_by_type <- ggplot(CE_month_type_long, aes(y = count, x = Month, fi
 
 gg_vbar_month_by_type <- dml(ggobj = gg_vbar_month_by_type, bg = "transparent")
 
-#########################
-### s2 CHART 2 horiz bars mschart
-
+### s2 ms barchart
 CE_grade <- read_excel(excel_fn, sheet = "CE Grade", col_names = TRUE) %>%  mutate(Type = factor(Type, levels = Type))
 
 CE_grade_long <- CE_grade %>% pivot_longer(cols = 2:dim(CE_grade)[2], names_to = "grade", values_to = "count")
-
-# slide4_gg_hbars <- ggplot(CE_grade_long, aes(x = count, y = reorder(Type, -count, sum, na.rm=TRUE), fill = grade)) + 
-#   geom_bar(position = position_fill(reverse = TRUE), stat="identity", color = "white", width = .45) +
-#   geom_text(aes(label = grade, fontface = "bold"),  position = position_fill(reverse = TRUE, vjust = .5), color = "white", size = 4.75) +
-#   labs(x = "", y = "", fill = "", title = "CE grade") +
-#   theme_minimal() +
-#   scale_fill_manual(values = oi_colors[1:6], drop = FALSE) +
-#   theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(),
-#         axis.text = element_blank(), axis.ticks = element_blank(),
-#         legend.position = "none", 
-#         plot.title = element_blank(),
-#         panel.background = element_rect(fill = "transparent", color = NA), plot.background = element_rect(fill = "transparent", color = NA))
-# 
-# slide4_gg_hbars <- dml(ggobj = slide4_gg_hbars, bg = "transparent")
 
 ms_hbars_grade <- ms_barchart(data = CE_grade_long, x = "Type", y = "count", group = "grade") %>%
   chart_settings(., dir="horizontal", grouping="percentStacked", gap_width = 150, overlap = 100 ) %>%
@@ -170,7 +121,6 @@ ms_hbars_grade <- ms_barchart(data = CE_grade_long, x = "Type", y = "count", gro
   chart_ax_x(.,major_tick_mark = "none", minor_tick_mark = "none", display = FALSE, tick_label_pos = "none") %>%
   chart_ax_y(.,major_tick_mark = "none", minor_tick_mark = "none", display = FALSE, tick_label_pos = "none") %>%
   chart_theme(., legend_position = "n", grid_major_line = fp_border(style = "none"), grid_major_line_y = fp_border(style = "none"))
-
 #print(slide4_ms_bars, preview = TRUE)
 
 ## S2 ppt out
@@ -183,15 +133,14 @@ ppt_out <- add_slide(ppt_out, layout = "slide2", master = "Theme1") %>%
   ph_with(value = ms_hbars_grade, location = ph_location_label(ph_label = "graph2")) %>%
   set_notes(value = slide2_notes, location = notes_location_type("body"))
 
-#################
+########################
 ## SLIDE 3 ggplot
-#################
+########################
 
 slide3_notes <- "Slide 2 features static plots made with ggplot2.
 Without wrapping the ggplots in dml(), the plots are saved as static image files in the final powerpoint."
 
-#########################
-### s3 CHART 2 horiz bar - outcome
+### s3 ggplot horiz bar
 CE_outcomes <- read_excel(excel_fn, sheet = "CE Outcome", col_names = TRUE) 
 
 gg_hbar_outcome <- ggplot(CE_outcomes, aes(x = `Outcome Count`, y = "", fill = reorder(`Outcome Label`, -`Outcome Count`))) + geom_bar(position = position_fill(reverse = TRUE), stat="identity", color = "white") +
@@ -205,10 +154,7 @@ gg_hbar_outcome <- ggplot(CE_outcomes, aes(x = `Outcome Count`, y = "", fill = r
         rect = element_rect(fill = "transparent")) + 
   guides(fill=guide_legend(nrow=3, byrow = TRUE))
 
-#gg_hbar_outcome <- dml(ggobj = gg_hbar_outcome, bg = "transparent")
-
-#########################
-### s3 CHART 2 table - actions
+### s3 ggplot table
 CE_actions <- read_excel(excel_fn, sheet = "Actions", col_names = TRUE) 
 
 CE_actions_long <- CE_actions %>% pivot_longer(cols = 2:dim(CE_actions)[2], names_to = "type", values_to = "count") %>%
@@ -239,9 +185,9 @@ ppt_out <- add_slide(ppt_out, layout = "slide3", master = "Theme1") %>%
   ph_with(value = gg_table_actions, location = ph_location_label(ph_label = "graph2")) %>%
   set_notes(value = slide3_notes, location = notes_location_type("body"))
 
-#############
+####################
 ## Save ppt
-##############
+#####################
 
 ppt_out_fn <- paste0("Cat Encounters_", report_type_short, " Numbers_", format(report_params$Start_Date, '%d-%b-%Y'), " to ", format(report_params$End_Date, '%d-%b-%Y'), ".pptx")
 print(ppt_out, target = ppt_out_fn)
